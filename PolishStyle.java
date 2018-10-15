244d71f5859dd7fca571a8846055cd39372970c4
JessicaFavin <jessica00228@hotmail.fr>
2018-10-15 15:32:54 +0200

    Command line interpretor

--- PolishStyle.java
+++ PolishStyle.java
@@ -1,17 +1,47 @@
+import java.util.Scanner;
+
 public class PolishStyle {
   public static void main(String[] args) {
     Stack stack = new Stack();
-    Stackable obj1 = new Stackable(6);
-    Stackable obj2 = new Stackable(3);
-    stack.push(obj1);
-    System.out.println(stack);
-    stack.push(obj2);
-    System.out.println(stack);
-    stack.push(obj1);
-    System.out.println(stack);
-    stack.push(obj2);
-    System.out.println(stack);
-    stack.div();
-    System.out.println(stack);
+    Scanner sc = new Scanner(System.in);
+    String input;
+    boolean stop = false;
+    System.out.println("Press q to quit");
+    System.out.println("Anything other than number or operand will be ignored");
+    ask: while (!stop) {
+      input = sc.nextLine();
+      if(input.equals("q")) {
+        stop = true;
+        break ask;
+      }
+      String[] parts = input.split(" ");
+      for(String s : parts) {
+        try{
+          int value = Integer.parseInt(s);
+          Stackable obj = new Stackable(value);
+          stack.push(obj);
+        } catch(NumberFormatException nfe) {
+          //nfe.printStackTrace();
+          String operator = s;
+          switch(operator) {
+            case "+":
+              stack.add();
+              break;
+            case "-":
+              stack.sub();
+              break;
+            case "*":
+              stack.mult();
+              break;
+            case "/":
+              stack.div();
+              break;
+            default:
+              break;
+          }
+        }
+      }
+      System.out.println(stack);
+    }
   }
 }
