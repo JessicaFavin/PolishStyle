5d45e57f8fc5c6bf5ea5d32b87c896792def9a4c
JessicaFavin <jessica00228@hotmail.fr>
2018-10-15 16:40:38 +0200

    Handle exceptions in Stack

---
+++ NotEnoughOperandsException.java
@@ -0,0 +1,5 @@
+public class NotEnoughOperandsException extends Exception {
+ public NotEnoughOperandsException() {
+
+ }
+}
--- Stack.java
+++ Stack.java
@@ -13,42 +13,65 @@ public class Stack {
       stack[stackPointer++] = obj;
     }
   }
+  public boolean isEmpty() {
+    return stackPointer<=0;
+  }
+
+  public boolean enoughOperand() {
+    return stackPointer>=2;
+  }
 
   public Stackable pop() {
     Stackable x = null;
-    if(stackPointer>0){
+    if(!this.isEmpty()){
       x = stack[stackPointer-1];
       stack[--stackPointer] = null;
     }
     return x;
   }
 
-  public void add() {
-    Stackable x = this.pop();
-    Stackable y = this.pop();
-    Stackable result = y.add(x);
-    this.push(result);
+  public void add() throws NotEnoughOperandsException{
+    if(this.enoughOperand()){
+      Stackable x = this.pop();
+      Stackable y = this.pop();
+      Stackable result = y.add(x);
+      this.push(result);
+    } else {
+      throw new NotEnoughOperandsException();
+    }
   }
 
-  public void sub() {
-    Stackable x = this.pop();
-    Stackable y = this.pop();
-    Stackable result = y.sub(x);
-    this.push(result);
+  public void sub() throws NotEnoughOperandsException{
+    if(this.enoughOperand()){
+      Stackable x = this.pop();
+      Stackable y = this.pop();
+      Stackable result = y.sub(x);
+      this.push(result);
+    } else {
+      throw new NotEnoughOperandsException();
+    }
   }
 
-  public void mult() {
-    Stackable x = this.pop();
-    Stackable y = this.pop();
-    Stackable result = y.mult(x);
-    this.push(result);
+  public void mult() throws NotEnoughOperandsException{
+    if(this.enoughOperand()){
+      Stackable x = this.pop();
+      Stackable y = this.pop();
+      Stackable result = y.mult(x);
+      this.push(result);
+    } else {
+      throw new NotEnoughOperandsException();
+    }
   }
 
-  public void div() {
-    Stackable x = this.pop();
-    Stackable y = this.pop();
-    Stackable result = y.div(x);
-    this.push(result);
+  public void div() throws NotEnoughOperandsException{
+    if(this.enoughOperand()){
+      Stackable x = this.pop();
+      Stackable y = this.pop();
+      Stackable result = y.div(x);
+      this.push(result);
+    } else {
+      throw new NotEnoughOperandsException();
+    }
   }
 
   @Override
